504de6e02bf9aad0153a6a98a7814d0a9c6aa665
mhimuro <h.mikuto@gmail.com>
2021-09-13 17:14:38 +0900

    aws-exportsの指定誤りを修正

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import config from './aws-exports.js';
 Amplify.configure(config);
 
 ReactDOM.render(
